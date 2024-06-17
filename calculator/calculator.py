6fa44b487c02209de31537e8663ebda7933a861e
DESKTOP-C5NM46J\Admin <aokiyan05@gmail.com>
2024-06-17 16:11:41 +0200

    V.1

---
+++ calculator/calculator.py
@@ -0,0 +1,20 @@
+résultat=0
+a=input("quelle est l'opération que tu veut faire?")
+if a==("+"):
+    b=int(input("(première valeur)"))
+    print("+")
+    c=int(input("(deuxième valeur)"))
+    résultat=b+c
+    print(résultat)
+elif a==("-"):
+    b=int(input("(première valeur)"))
+    print("-")
+    c=int(input("(deuxième valeur)"))
+    résultat=b-c
+    print(résultat)
+else:
+    b=int(input("(première valeur)"))
+    print("x")
+    c=int(input("(deuxième valeur)"))
+    résultat=b*c
+    print(résultat)
