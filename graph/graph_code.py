839e41413e22eefdd4245da529ba88e3e1761b75
DESKTOP-C5NM46J\Admin <aokiyan05@gmail.com>
2024-06-25 10:54:11 +0200

    début du code

---
+++ graph/graph_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#--coding: UTF-8--
+
+"""
+@file       graph_code.py
+@copyright  InSolem SARL reserves all rights even in the event of industrial
+            property rights. We reserve all rights of disposal such as
+            copying and passing on to third parties.
+"""
+
+import matplotlib as plt    # import the matplotlib library and rename it plt
+
+plt.clf()                   # erase the old charts
+plt.show()                  # show the chart
+plt.xlabel('x')
+plt.ylabel('y')
