82206ca00b6dd38b9cecdcd23d7ef3e708220b36
DESKTOP-C5NM46J\Admin <aokiyan05@gmail.com>
2024-06-25 11:32:27 +0200

    YTD-9 : version fonctionnelle de graphe un peu modifiée

--- graph/graph_code.py
+++ graph/graph_code.py
@@ -8,9 +8,36 @@
             copying and passing on to third parties.
 """
 
-import matplotlib as plt    # import the matplotlib library and rename it plt
+import numpy as np                      # import the numpy library and rename it np
+from matplotlib import pyplot as plt    # import from the matplotlib library the fonction pyplot and rename it plt
 
-plt.clf()                   # erase the old charts
-plt.show()                  # show the chart
-plt.xlabel('x')
-plt.ylabel('y')
+## init
+#plt.clf()                   # erase the old charts
+#plt.title('la base')        # add a title to the graph
+#plt.show()                  # show the chart
+#plt.xlabel('x')             # name the x axis "x"
+#plt.ylabel('y')             # name the y axis "y"
+#plt.grid()                  # add a grid to the chart
+#
+#def fonction():
+#
+#    
+#
+#
+#
+#plt.plot(fonction)
+
+
+
+
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+
+def f(x):
+   return np.sin(x)*x/x*x
+
+x = np.linspace(-1000, 1000, 1000)
+
+plt.plot(x, f(x), color='blue')
+plt.grid()
+plt.show()
