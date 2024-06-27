5f3373a674ad27b022163df97dd37a3baf11e13f
DESKTOP-C5NM46J\Admin <aokiyan05@gmail.com>
2024-06-27 16:07:45 +0200

    améloration du code en quelque chose de fonctionnel mais sans graphe

--- accéléromètre/graph_accel.py
+++ accéléromètre/graph_accel.py
@@ -3,9 +3,13 @@ import time
 import numpy as np                      # import the numpy library and rename it np
 from matplotlib import pyplot as plt    # import from the matplotlib library the fonction pyplot and rename it plt
 
+ser_port = None
 
 def init_uart():
-    pass
+    global ser_port
+    ser_port  =  serial.Serial(port  =  "COM3" , baudrate = 9600 , timeout = 2 )
+    if  (ser_port.isOpen()  ==  False):
+        ser_port. open ()                                       # check and open Serial0
 
 def init_graph():
     fig = plt.figure()                  # Creating an empty figure or plot
@@ -14,13 +18,13 @@ def init_graph():
 
     
 def read_uart():
-    ser  =  serial.Serial(port  =  "COM3" , baudrate = 9600 , timeout = 2 )
-    if  (ser.isOpen()  ==  False):
-        ser. open ()                                       # check and open Serial0
-    ser.flushInput()                                       # clear the UART Input buffer
-    ser.flushOutput()                                      # clear the UART output buffer                           
-    bs = ser.readline().decode('utf-8').strip()
+    global ser_port
+    ser_port.flushInput()                                       # clear the UART Input buffer
+    ser_port.flushOutput()                                      # clear the UART output buffer                           
+    bs = ser_port.readline().decode('utf-8').strip()
     print(repr(bs))
+
+    return bs
 
 def parse_accel(txt):
     a = txt.split()
@@ -34,9 +38,10 @@ def update_graph():
     pass
 
 if __name__ == "__main__":
+    init_uart()
     init_graph()
 
     while(1):
-        read_uart()
-        parse_accel()
+        texto = read_uart()
+        parse_accel(texto)
         update_graph()
